3102000bb379e3aee5a0391f306a5bbba86fbbb7
Diego Gutiérrez <66325409+TheWhiteLionXs@users.noreply.github.com>
2020-10-20 00:15:00 +0200

    Add files via upload

---
+++ Python__Minecraft__block.py
@@ -0,0 +1,17 @@
+#+++++++++++++++++++++++++++++++PYTHON MINECRAFT++++++++++++++++++++++++++++++++
+#Connect Minecraft with Python.
+from mcpi import block
+from mcpi.minecraft import Minecraft
+#Inportar librerias
+import time
+import math
+#-------------------------------------CODE--------------------------------------
+mc = Minecraft.create()
+#Crear un bucle
+while True:
+#Obtener las coordenadas del jugador.
+    x, y, z = mc.player.getTilePos()
+#Obtener el bloque de la cordenada del jugador.
+    b = mc.getBlock(x, y-1, z)
+    mc.postToChat(b)
+    time.sleep(1)
